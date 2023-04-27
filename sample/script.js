d56261db0e92daba018d6177321e761610878c22
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-04-27 23:11:12 +0530

    updates

---
+++ sample/script.js
@@ -0,0 +1,27 @@
+// function register(event) {
+//     event.preventDefault();
+//     // alert("Working...")
+//     var name = document.getElementById("userName").value;
+//     var email = document.getElementById("userEmail").value;
+//     var password = document.getElementById("userPassword").value;
+//     console.log(name, email, password, "check hereee");
+//     if (name && email && password) {
+//         alert("Registtration Done..")
+//     }
+
+// }
+
+// var myName = "Newton school";
+// console.log(myName, "first");
+// myName = "School Newton";
+// console.log(myName, "second")
+
+// const [      fir,          sec] = useState("School newton");
+//       curent data, update the data              Data
+
+//       sec("School newton")
+
+window.onload = function () {
+    alert("Page rendered on Browser.")
+}
+
